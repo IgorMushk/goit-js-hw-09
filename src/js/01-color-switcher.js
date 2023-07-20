0661ce951bce1d5b658449afb1f5bd9dea7e7c6d
IgorMushk <igor.mushketik@gmail.com>
2023-07-20 12:53:56 +0300

    Ex.1 add disabled buttons

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,18 +2,22 @@ const bodyColor = document.querySelector('body');
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 
-let timeId = null;
+let timeId = 0;
+
+stopBtn.disabled = true;
 
 startBtn.addEventListener('click', () => {
   timeId = setInterval(() => {
     bodyColor.style.backgroundColor = getRandomHexColor();
     startBtn.disabled = true;
+    stopBtn.disabled = false;
   }, 1000);
 });
 
 stopBtn.addEventListener('click', () => {
   clearInterval(timeId);
   startBtn.disabled = false;
+  stopBtn.disabled = true;
 });
 
 function getRandomHexColor() {
