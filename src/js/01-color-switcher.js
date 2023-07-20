bd3e77f5cd8d2b01bd6595129ff743a7ec84fde0
IgorMushk <igor.mushketik@gmail.com>
2023-07-20 12:41:43 +0300

    Ex.1  colorBtn  - done as written

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyColor = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+// console.log(startBtn);
+// console.log(stopBtn);
 
+let timeId = null;
+
+startBtn.addEventListener('click', () => {
+  timeId = setInterval(() => {
+    //console.log('color :', getRandomHexColor());
+    bodyColor.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timeId);
+  startBtn.disabled = false;
+  //console.log('stop', timeId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
