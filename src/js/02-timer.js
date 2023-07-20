385e8ab1e1a4e0e60295d88e062e04f399bc5fca
IgorMushk <igor.mushketik@gmail.com>
2023-07-20 20:52:10 +0300

    Ex.2 - done as written in technical task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,7 @@
 import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const inputCalendar = document.querySelector('input#datetime-picker');
 const startBtn = document.querySelector('button[data-start]');
@@ -10,12 +11,18 @@ const leftDay = document.querySelector('span[data-days]');
 const leftHours = document.querySelector('span[data-hours]');
 const leftMinutes = document.querySelector('span[data-minutes]');
 const leftSeconds = document.querySelector('span[data-seconds]');
-console.log(leftDay);
+//console.log(leftDay);
 
-leftDay.textContent = '01';
-leftHours.textContent = '02';
-leftMinutes.textContent = '03';
-leftSeconds.textContent = '04';
+// leftDay.textContent = '01';
+// leftHours.textContent = '02';
+// leftMinutes.textContent = '03';
+// leftSeconds.textContent = '04';
+
+let timeId = 0;
+startBtn.disabled = true;
+let selectDate;
+let delayTime;
+let carentTime;
 
 const options = {
   enableTime: true,
@@ -23,7 +30,20 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    //console.log(selectedDates[0]);
+    const carentDate = new Date();
+    //console.log(carentDate);
+    //console.log(selectedDates[0] - carentDate);
+    deltaTime = new Date(selectedDates[0] - carentDate).getTime();
+    console.log(deltaTime);
+    if (deltaTime > 0) {
+      startBtn.disabled = false;
+      selectedTime = selectedDates[0];
+      startBtn.addEventListener('click', onClickStart);
+    } else {
+      //alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
+    }
   },
 };
 
@@ -31,3 +51,46 @@ const options = {
 //console.log(selectDate);
 //flatpickr('input#datetime-picker', options);
 flatpickr(inputCalendar, options);
+
+function onClickStart() {
+  timeId = setInterval(() => {
+    carentTime = new Date();
+    delayTime = selectedTime - carentTime;
+    //console.log(delayTime);
+    if (delayTime < 0) {
+      Notify.success('Time is over');
+      startBtn.disabled = true;
+      clearInterval(timeId);
+      return;
+    }
+    leftDay.textContent = convertMs(delayTime).days.toString().padStart(2, '0');
+    leftHours.textContent = convertMs(delayTime)
+      .hours.toString()
+      .padStart(2, '0');
+    leftMinutes.textContent = convertMs(delayTime)
+      .minutes.toString()
+      .padStart(2, '0');
+    leftSeconds.textContent = convertMs(delayTime)
+      .seconds.toString()
+      .padStart(2, '0');
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
