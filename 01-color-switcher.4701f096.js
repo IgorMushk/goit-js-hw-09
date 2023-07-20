906567637d87c4e2ea096539a90008a767582846
IgorMushk <127196231+IgorMushk@users.noreply.github.com>
2023-07-20 09:42:02 +0000

    Deploying to gh-pages from @ IgorMushk/goit-js-hw-09@bd3e77f5cd8d2b01bd6595129ff743a7ec84fde0 🚀

---
+++ 01-color-switcher.4701f096.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(()=>{o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,e.disabled=!0}),1e3)})),a.addEventListener("click",(()=>{clearInterval(o),e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.4701f096.js.map
